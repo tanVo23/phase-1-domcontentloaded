049482617dee7b986c30d191704aa85a44964c7e
Tan <tan10tvo@gmail.com>
2021-06-21 17:41:12 -0600

    commit tree c3b2ac058a53afa12b85256a26446a696bcb5f5a

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("Dom Content Has Loaded");
+    document.getElementById('text').textContent = "This is really cool!";
+});
+
+
